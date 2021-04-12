7c05a30ffb170060139373ad157fd54fdb475e96
richardingwe <richardingwe5@gmail.com>
2021-04-12 18:57:07 +0100

    worked on refreshToken

--- src/useAuth.js
+++ src/useAuth.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const useAuth = (code) => {
@@ -21,6 +21,28 @@ const useAuth = (code) => {
 				window.location = '/';
 			});
 	}, [code]);
+
+	useEffect(() => {
+		if (!refreshToken || !expiresIn) return;
+
+		const interval = setInterval(() => {
+			axios
+				.post('http://localhost:3001/refresh', {
+					refreshToken,
+				})
+				.then((res) => {
+					setAcessToken(res.data.accessToken);
+					setRefreshToken(res.data.refreshToken);
+					setExpiresIn(res.data.expiresIn);
+					window.history.pushState({}, null, '/');
+				})
+				.catch(() => {
+					window.location = '/';
+				});
+		}, (expiresIn - 60) * 1000);
+
+		return () => clearInterval(interval);
+	}, [refreshToken, expiresIn]);
 
 	return accessToken;
 };
