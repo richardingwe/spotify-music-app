9e516e860de75c60ad42402a69c12e84be8c78cb
richardingwe <richardingwe5@gmail.com>
2021-04-20 23:58:50 +0100

    added a comment

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import useAuth from './useAuth';
 
 const Dashboard = ({ code }) => {
+	// acess token
 	const accessToken = useAuth(code);
 	return <div>{code}</div>;
 };
