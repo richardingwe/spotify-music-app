3b2920b00122b5a19b3c7b1122a776d63cc88e8a
richardingwe <richardingwe5@gmail.com>
2021-04-21 23:29:57 +0100

    added search bar

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, Form } from 'react-bootstrap';
 import useAuth from './useAuth';
 
 const Dashboard = ({ code }) => {
 	// acess token
 	const accessToken = useAuth(code);
-	return <div>{code}</div>;
+	const [search, setSearch] = useState('');
+	return (
+		<Container
+			className='d-flex flex-column py-2'
+			style={{
+				height: '100vh',
+			}}
+		>
+			<Form.Control
+				type='search'
+				placeholder='Search Songs/Artists'
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+			/>
+			<div
+				className='flex-grow-1 my-2'
+				style={{
+					overflowY: 'auto',
+				}}
+			>
+				Songs
+			</div>
+			<div>Bottom</div>
+		</Container>
+	);
 };
 
 export default Dashboard;
